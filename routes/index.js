5bc2d76306e6dc855a1a4a964caf9709f686ac02
bryanparmelee <bryan.parmelee@gmail.com>
2023-02-09 16:41:10 -0500

    Add messages, form for adding new messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+const messages =  [
+  {
+    text: "Hello world!",
+    user: "Bobby",
+    added: new Date()
+  },
+  {
+    text: "I'm here!",
+    user: "Sally",
+    added: new Date()
+  },
+  {
+    text: "Allow me to introduce myself",
+    user: "Billy",
+    added: new Date()
+  },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-module.exports = router;
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Add a message' });
+})
+
+router.post('/new', function(req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.author;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  next();
+  res.redirect('/');
+})
+
+module.exports = router, messages;
